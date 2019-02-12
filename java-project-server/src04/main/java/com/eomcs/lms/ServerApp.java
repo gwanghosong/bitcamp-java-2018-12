d815ab9e8abb913e4b543a61484b68ce7b256f2f
gwanghosong91@gmail.com <gwanghosong91@gmail.com>
2019-02-12 18:11:28 +0900

    2월 12일 최종

---
+++ java-project-server/src04/main/java/com/eomcs/lms/ServerApp.java
@@ -0,0 +1,42 @@
+// 3단계
+package com.eomcs.lms;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import com.eomcs.lms.domain.Member;
+
+public class ServerApp {
+
+  public static void main(String[] args) {
+
+    try (ServerSocket serverSocket = new ServerSocket(8888)) {
+      System.out.println("서버 시작");
+
+      while (true) {
+        try (Socket socket = serverSocket.accept();
+            ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+            ObjectInputStream in = new ObjectInputStream((socket.getInputStream()))) {
+
+          System.out.println("클라이언트와 연결되었음.");
+
+          // 클라이언트에서 serialize해서 보내온 Member 객체의 내용을 출력하라. 
+          Member request = (Member) in.readObject();
+          
+          // 그리고 즉시 클라이언트로 Member 객체를 serialize하여 보내라!
+          out.writeObject(request);
+          out.flush();
+
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+        System.out.println("클라이언트와의 연결을 끊었음.");
+      }
+
+    } catch (Exception e) {
+      e.printStackTrace();
+
+    }
+  }
+}
