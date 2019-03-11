4b2c1279c0cee45c2c904578089e22600271136b
gwanghosong91@gmail.com <gwanghosong91@gmail.com>
2019-03-11 18:30:09 +0900

    3월 11일

--- java-project2-server/src/main/java/com/eomcs/lms/handler/PhotoBoardAddCommand.java
+++ java-project2-server/src/main/java/com/eomcs/lms/handler/PhotoBoardAddCommand.java
@@ -1,23 +1,31 @@
 package com.eomcs.lms.handler;
-import com.eomcs.lms.ApplicationInitializer;
+import java.util.ArrayList;
 import com.eomcs.lms.dao.PhotoBoardDao;
 import com.eomcs.lms.dao.PhotoFileDao;
 import com.eomcs.lms.domain.PhotoBoard;
 import com.eomcs.lms.domain.PhotoFile;
+import com.eomcs.mybatis.TransactionManager;
 
 public class PhotoBoardAddCommand extends AbstractCommand {
-
+  
+  TransactionManager txManager;
   PhotoBoardDao photoBoardDao; // 서버의 PhotoBoardDaoImpl 객체를 대행하는 프록시 객체이다.
   PhotoFileDao photoFileDao;
 
-  public PhotoBoardAddCommand(PhotoBoardDao photoBoardDao, 
-      PhotoFileDao photoFileDao) {
+  public PhotoBoardAddCommand(
+      PhotoBoardDao photoBoardDao, 
+      PhotoFileDao photoFileDao,
+      TransactionManager txManager) {
     this.photoBoardDao = photoBoardDao;
     this.photoFileDao = photoFileDao;
+    this.txManager = txManager;
   }
 
   @Override
   public void execute(Response response) throws Exception {
+    txManager.beginTransaction();
+    
+    try {
     PhotoBoard photoBoard = new PhotoBoard();
     photoBoard.setTitle(response.requestString("사진 제목?"));
     photoBoard.setLessonNo(response.requestInt("수업번호?"));
@@ -26,11 +34,11 @@ public class PhotoBoardAddCommand extends AbstractCommand {
     response.println("최소 한 개의 사진 파일을 등록해야 합니다.");
     response.println("파일명 입력 없이 그냥 엔터를 치면 파일 추가를 마칩니다.");
 
-    int count = 0;
+    ArrayList<PhotoFile> files = new ArrayList<>();
     while (true) {
       String filePath = response.requestString("사진 파일?");
       if (filePath.length() == 0) {
-        if (count == 0) {
+        if (files.size() == 0) {
           response.println("최소 한개의 사진 파일을 등록해야 합니다.");
           continue;
         } else {
@@ -41,17 +49,20 @@ public class PhotoBoardAddCommand extends AbstractCommand {
       PhotoFile file = new PhotoFile();
       file.setFilePath(filePath);
       file.setPhotoBoardNo(photoBoard.getNo());
-
-      photoFileDao.insert(file);
-      count++;
-
+      
+      files.add(file);
     }
-
+    
+    photoFileDao.insert(files);
+    
     response.println("저장하였습니다.");
-
-    // 이제는 여기서 직접 처리하는 것이 아니라 모든 핸들러 커맨드들이 상속받는
-    // AbstractCommand 클래스에서 처리
-    //ApplicationInitializer.con.commit();
+    txManager.commit();
+    
+    } catch (Exception e) {
+      e.printStackTrace();
+      response.println("저장 중 오류 발생");
+      txManager.rollback();
+    }
   }
 }
 
